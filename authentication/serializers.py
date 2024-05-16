1ebdb999aca02badbbe2d8c3a11797a3043961ae
donglam1824 <donglam0378378@gmail.com>
2024-05-16 10:51:58 +0700

    chu thich

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -4,25 +4,25 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
-class UserSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):   #chuyen model User sang dang JSON va nguoc lai
     class Meta:
         model = User
         fields = ('username', 'email', 'first_name', 'last_name', 'password', 'is_active')
         extra_kwargs = {'password': {'write_only': True}}
 
-    def create(self, validated_data):
+    def create(self, data):
         user = User(
-            email=validated_data['email'],
-            username=validated_data['username'],
-            first_name=validated_data['first_name'],
-            last_name=validated_data['last_name'],
+            email=data['email'],
+            username=data['username'],
+            first_name=data['first_name'],
+            last_name=data['last_name'],
         )
-        user.set_password(validated_data['password'])
+        user.set_password(validate=data['password'])
         user.is_active = True  # Set the user to be active by default
         user.save()
         return user
 
-
+#override
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
     def get_token(cls, user):
