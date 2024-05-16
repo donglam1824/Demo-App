e19b3064b3db1c0e513bedff3527e596e351a729
donglam1824 <donglam0378378@gmail.com>
2024-05-16 10:38:30 +0700

    active user

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -7,7 +7,21 @@ from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'first_name', 'last_name')
+        fields = ('username', 'email', 'first_name', 'last_name', 'password', 'is_active')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User(
+            email=validated_data['email'],
+            username=validated_data['username'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+        )
+        user.set_password(validated_data['password'])
+        user.is_active = True  # Set the user to be active by default
+        user.save()
+        return user
+
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
